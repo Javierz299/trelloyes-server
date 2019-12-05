ceaab3acdbf0015222013ccfe5c8a006b7bf8662
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-05 15:29:51 -0500

    added get by id handlers for card and list. autho verification works as well.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston')
 const { NODE_ENV } = require('./config')
 
 const app = express()
@@ -14,12 +15,84 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
+
+const cards = [{
+  id: 1,
+  title: 'Task One',
+  content: 'This is card one'
+}];
+const lists = [{
+  id: 1,
+  header: 'List One',
+  cardIds: [1]
+}];
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({filename: 'info.log'})
+  ]
+})
 
 app.get('/', (req,res) => {
     res.send('Hello, world!')
 })
 
+
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`)
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+//////////////CARD/////////////
+app.get('/card',(req,res) => {
+  res.json(cards)
+})
+
+app.get('/card/:id',(req,res) => {
+  const { id } = req.params;
+  const card = cards.find(c => c.id == id);
+
+  if (!card) {
+    logger.error(`Card with id ${id} not found.`);
+    return res
+      .status(404)
+      .send('Card Not Found');
+  }
+
+  res.json(card);
+})
+
+//////////////LIST////////////
+app.get('/list',(req,res) => {
+  res.json(lists)
+})
+
+app.get('/list/:id', (req,res) => {
+  const { id }  = req.params;
+  const list = lists.find(li => li.id == id);
+
+  if(!list){
+    logger.error(`List with id ${id}  not found`);
+    return res.status(404).send('List Not Found')
+  }
+  res.json(list)
+})
+
+
 app.use(function errorHandler(error, req, res, next) {
+
        let response
        if (NODE_ENV === 'production'){
          response = { error: { message: 'server error' } }
